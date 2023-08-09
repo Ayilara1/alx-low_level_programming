f6c9e8cfc41c19b30ee0039f2c236afb53955068
Ayilara Busari <toyin_lboy@hotmail.com>
2023-08-09 08:52:51 +0000

    yes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Can't write to %s\n", file);
+		"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -39,7 +39,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close 		   fd %d\n", fd);
 		exit(100);
 	}
 }
